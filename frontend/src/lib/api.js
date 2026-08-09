ccb1738841b1084441cd9f4cd7e800b853730507
balaram17 <balaram17saragada@gmail.com>
2026-08-09 22:36:40 +0530

    Fix Azure backend base URL

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'https://ramsboutique-api-prod-endcc7bmbegsanca.southindia-01.azurewebsites.net';
 export const API = `${BACKEND_URL}/api`;
 
-const api = axios.create({ baseURL: process.env.REACT_APP_API_URL, });
+const api = axios.create({ 
+  baseURL: API,
+  headers: {
+    'Content-Type': 'application/json',
+  }
+ });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('rb_token');
